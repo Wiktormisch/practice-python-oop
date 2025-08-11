f0b7840d298a4f3175d6ac53d5441b75403a673e
Wiktor Mischker <wiktor.misch@gmnail.com>
2025-08-11 13:29:09 +0200

    name folder fix

--- 01/person.py
+++ 01/person.py
@@ -1,4 +1,4 @@
-class person:
+class Person:
     def __init__(self, name, age, city):
         self.name = name
         self.age = age
@@ -7,9 +7,9 @@ class person:
     def introduce(self):
         print("Czesc, mam na imie",self.name, ",mam",self.age, "lat i mieszkam w",self.city)
 
-person0 = person("Maciej",17,"Sopot")
-person1 = person("Kasia",23,"Gdynia")
-person2 = person("Krystian",37,"Gdansk")
+person0 = Person("Maciej",17,"Sopot")
+person1 = Person("Kasia",23,"Gdynia")
+person2 = Person("Krystian",37,"Gdansk")
 
 person0.introduce()
 person1.introduce()
